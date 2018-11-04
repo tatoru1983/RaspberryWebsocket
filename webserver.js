af34511bb9c2f4a69ca764715a0f76beaab00342
tatoru1983 <37672216+tatoru1983@users.noreply.github.com>
2018-11-04 23:32:51 +0100

    first commit

---
+++ webserver.js
@@ -0,0 +1,53 @@
+var http = require('http').createServer(handler); //require http server, and create server with function handler()
+var fs = require('fs'); //require filesystem module
+var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
+
+var Gpio = require('onoff').Gpio;
+var rly1 = new Gpio(17, 'out');
+var rly2 = new Gpio(18, 'out');
+
+http.listen(8080); //listen to port 8080
+
+function handler (req, res) { //create server
+  fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+    res.write(data); //write data from index.html
+    return res.end();
+  });
+}
+
+io.sockets.on('connection', function (socket) {// WebSocket Connection
+  var lightvalue = 0; //static variable for current status
+  
+  socket.on('up', function(data) { //get light switch status from client
+    lightvalue = data;
+    console.log(lightvalue);
+	rly1.writeSync(0);
+	rly2.writeSync(1);
+  });
+  
+  socket.on('down', function(data) { //get light switch status from client
+    lightvalue = data;
+    console.log(lightvalue);
+	rly2.writeSync(0);
+	rly1.writeSync(1);
+  });
+  
+  socket.on('stop', function(data) { //get light switch status from client
+    lightvalue = data;
+    console.log(lightvalue);
+	rly1.writeSync(0);
+	rly2.writeSync(0);
+  });
+});
+
+process.on('SIGINT', function () { //on ctrl+c
+  rly1.writeSync(0);
+  rly2.writeSync(0);
+  console.log('process.exit');
+  process.exit(); //exit completely
+});
